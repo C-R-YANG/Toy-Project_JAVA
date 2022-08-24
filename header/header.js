3f16ca22e41ac7b764fa6274bf014b2dc6f218d2
초롱\User <yxchxxll@gmail.com>
2022-08-24 23:22:30 +0900

    화면별 Structor 변경 C.R.YANG

---
+++ header/header.js
@@ -0,0 +1,11 @@
+const menuList = document.querySelectorAll(".menu_list > div");
+
+menuList.forEach(menu => {
+  menu.addEventListener("click", function(){
+    importContentPage(this.innerHTML);
+  });
+});
+
+document.querySelector("#move_home").addEventListener("click", function(){
+  importContentPage("home");
+})
---
+++ index/index.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function(){
+  importPage("header");
+  importContentPage("home");
+  importPage("footer");
+});
+
+async function fetchHtmlAsText(url) {
+  return await (await fetch(url)).text();
+}
+
+async function importPage(target) {
+  const tag       = document.querySelector("#" + target),
+        newScript = document.createElement("script");
+
+  tag.innerHTML = await fetchHtmlAsText("../" + target + "/" + target + ".html");
+
+  newScript.src = "../" + target + "/" + target + ".js"; 
+  
+  tag.appendChild(newScript);
+}
+
+async function importContentPage(target) {
+  const content   = document.querySelector("#content"),
+        newScript = document.createElement("script");
+
+  content.innerHTML = await fetchHtmlAsText("../" + target + "/" + target + ".html");
+
+  newScript.src = "../" + target + "/" + target + ".js";
+
+  content.appendChild(newScript);
+}
