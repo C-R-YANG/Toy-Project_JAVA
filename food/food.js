19054ead5c02cef6d5013ab338a78219b56c44f5
초롱\User <yxchxxll@gmail.com>
2022-08-26 23:16:31 +0900

    [FOOD페이지] 서치바 이벤트 추가 C.R.YANG

--- food/food.js
+++ food/food.js
@@ -1,73 +1,73 @@
-const areaDropdown = document.querySelector(".area_dropdown_content");
-const sortDropdown = document.querySelector(".sort_dropdown_content");
-const parkingDropdown = document.querySelector(".parking_dropdown_content");
+function showDropDown(obj) {
+  const dropDown = obj.parentNode.children[3];
+  
+  dropDown.classList.add("block");
+  dropDown.classList.remove("none");
+}
 
-document.querySelector(".area_btn").addEventListener(
-  "click",
-  function () {
-    areaDropdown.classList.add("block");
+function hideDropDown(obj) {
+  const dropDown = obj.parentNode;
 
-    areaDropdown.classList.remove("none");
-  },
-  false
-);
-
-document.querySelector(".area_apply_btn").addEventListener(
-  "click",
-  function () {
-    areaDropdown.classList.add("none");
-
-    areaDropdown.classList.remove("block");
-  },
-  false
-);
-
-document.querySelector(".sort_btn").addEventListener(
-  "click",
-  function () {
-    sortDropdown.classList.add("block");
-
-    sortDropdown.classList.remove("none");
-  },
-  false
-);
-
-document.querySelector(".sort_apply_btn").addEventListener(
-  "click",
-  function () {
-    sortDropdown.classList.add("none");
-
-    sortDropdown.classList.remove("block");
-  },
-  false
-);
-
-document.querySelector(".parking_btn").addEventListener(
-  "click",
-  function () {
-    parkingDropdown.classList.add("block");
-
-    parkingDropdown.classList.remove("none");
-  },
-  false
-);
-
-document.querySelector(".parking_apply_btn").addEventListener(
-  "click",
-  function () {
-    parkingDropdown.classList.add("none");
-
-    parkingDropdown.classList.remove("block");
-  },
-  false
-);
+  dropDown.classList.add("none");
+  dropDown.classList.remove("block");
+}
 
 function array_click(obj) {
-  const parent = obj.parentNode;
-  const child = parent.children;
+  const parent = obj.parentNode,
+        childList  = parent.children;
 
-  for (var i = 0; i < child.length; i++) {
-    child[i].className = "array_list";
+  for (let i = 0; i < childList.length; i++) {
+    childList[i].className = "array_list";
   }
+
   obj.classList.add("array_list_click");
+}
+
+function allCheck(obj) {
+  const parent = obj.parentNode,
+        isChk  = obj.children[0].checked,
+        childList  = parent.querySelectorAll("input");
+  
+  for (let i = 0; i < childList.length; i++) {
+    childList[i].checked = isChk;
+  }
+}
+
+function refrestFilter() {
+  const searchWrap   = document.querySelector(".search_wrap"),
+        checkBoxList = searchWrap.querySelectorAll("input[type='checkbox']");
+  
+  for (let i = 0; i < checkBoxList.length; i++) {
+    checkBoxList[i].checked = true;
+  }
+
+  searchWrap.querySelector("#possible").checked = true;
+  searchWrap.querySelector("#both").checked = false;
+
+  searchWrap.querySelector(".keyword_input").value = ""
+}
+
+function isCheck(obj) {
+  const isCheck = obj.children[0].checked,
+        parent  = obj.parentNode,
+        allChkBox = parent.children[0].children[0];
+
+  if (!isCheck) {
+    allChkBox.checked = false;
+  } else {
+    const checkBox = parent.querySelectorAll("input[type='checkbox']");
+
+    let isChk = true;
+    for (let i = 1; i < checkBox.length; i++) {
+      isChk = checkBox[i].checked;
+      
+      if (!isChk) {
+        return false;
+      }
+    }
+
+    if (isChk) {
+      allChkBox.checked = true;
+    }
+  }
 }
