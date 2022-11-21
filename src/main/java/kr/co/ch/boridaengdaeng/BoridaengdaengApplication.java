647913069b9e19eb6feaaad1892bda29d13b0b36
초롱\User <yxchxxll@gmail.com>
2022-11-21 19:58:38 +0900

    [spring boot] 초기 환경 설정 작업 진행 중 C.R.YANG

---
+++ src/main/java/kr/co/ch/boridaengdaeng/BoridaengdaengApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ch.boridaengdaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoridaengdaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoridaengdaengApplication.class, args);
+	}
+
+}
